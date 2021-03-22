a5935b5fb0243abd2dc3ac0842493454a731edd8
Vishwasa <mail2vishwas.sl@gmail.com>
2021-03-23 01:20:10 +0530

    Empty Console Project created
    Empty Console Project created using visual studio template

---
+++ BooleanExpressionEvaluator/BooleanExpressionEvaluator/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BooleanExpressionEvaluator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
